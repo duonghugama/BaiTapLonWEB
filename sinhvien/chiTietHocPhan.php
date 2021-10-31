27721df8f48a080f3f104a152284f0e2db854f2e
minh7721 <nhatminh7721@gmail.com>
2021-11-01 01:22:15 +0700

    fix bug

--- sinhvien/chiTietHocPhan.php
+++ sinhvien/chiTietHocPhan.php
@@ -33,53 +33,55 @@ include("config/db.php");
     <!-- <section class="pb-4"> -->
     <!-- <div class="bg-light rounded-5"> -->
     <!-- Chọn kì học -->
-    <section class="ps-4 d-flex justify-content-start">
-        <div class="me-3" style="width: 15rem;">
-        <select class="form-select" data-mdb-visible-options="3" style="background-color: #f1efef;">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            </select>
-        </div>
-        <!-- Chọn năm học -->
-        <div style="width: 15rem;">
-           <form action="" method="POST">
-           <select class="form-select" data-mdb-visible-options="3" style="background-color: #f1efef;">
-                <?php
-                $sql_dsNamHoc = "SELECT * FROM khoahoc";
-                $rs_dsNamHoc = mysqli_query($conn, $sql_dsNamHoc);
-                if (mysqli_num_rows($rs_dsNamHoc) > 0) {
-                    $i = 1;
-                    while ($row_dsNamHoc = mysqli_fetch_assoc($rs_dsNamHoc)) {
-                ?>
-                        <option name="namHoc" value="<?php echo $i ?>"><?php echo $row_dsNamHoc['Ten'] ?></option>
-                <?php
-                        $i++;
+    <form action="" method="POST">
+        <section class="ps-4 d-flex justify-content-start">
+        <form action="" method="POST">
+        <section class="ps-4 d-flex justify-content-start">
+            <!-- Chọn năm học -->
+            <div style="width: 15rem;">
+                <select disabled name="namHoc" class="form-select" data-mdb-visible-options="3" style="background-color: #f1efef;">
+                    <?php
+                    $sql_dsNamHoc = "SELECT * FROM khoahoc";
+                    $rs_dsNamHoc = mysqli_query($conn, $sql_dsNamHoc);
+                    if (mysqli_num_rows($rs_dsNamHoc) > 0) {
+                        $i = 1;
+                        while ($row_dsNamHoc = mysqli_fetch_assoc($rs_dsNamHoc)) {
+                    ?>
+                            <option value="<?php echo $i ?>"><?php echo 'Kỳ '.$row_dsNamHoc['Ky'].' Năm '.$row_dsNamHoc['Ten'] ?></option>
+                    <?php
+                            $i++;
+                        }
                     }
-                }
-                ?>
-                <!-- <option value="1">Năm học 2020</option> -->
-            </select>
-           </form>
-        </div>
-    </section>
+                    ?>
+                    <!-- <option value="1">Năm học 2020</option> -->
+                </select>
+            </div>
+        </section>
+    </form>
+        </section>
+    </form>
 
     <div class="container-fluid">
         <div class="row">
             <ul class="list-group col-md-3 mt-5 ms-4">
                 <h4 style="font-weight: 500;">Môn học</h4>
                 <li class="list-group-item ps-5">
-                    <?php
-                    $sql_dsMon = "SELECT * FROM monhoc";
-                    $rs_dsMon = mysqli_query($conn, $sql_dsMon);
-                    if (mysqli_num_rows($rs_dsMon) > 0) {
-                        while ($row_dsMon = mysqli_fetch_assoc($rs_dsMon)) {
-                    ?>
-                            <!-- <input class="form-check-input me-1" name="checkDangKy" type="checkbox" value="" aria-label="..."> -->
-                            <?php
-                            echo "<a id='dkMonHoc' href='chiTietHocPhan.php?idMon=" . $row_dsMon['MaMon'] . "'>" . $row_dsMon['Ten'] . "</a>";
-                            echo "<br>";
-                            ?>
-                    <?php
+                <?php
+                        $namHoc = $_GET['namhoc'];
+                        $sql_dsMon = "select monhoc.MaMon as MaMonHoc, monhoc.Ten as tenMon , ThoiGianBatDau, ThoiGianKetThuc, tietBatDau, tietKetThuc, phongHoc, giaovien.Ten
+                        from chitietkhoahoc , monhoc , khoahoc, giaovien
+                         where chitietkhoahoc.MaKH = khoahoc.MaKH and chitietkhoahoc.MaMon = monhoc.MaMon
+                         and giaovien.MaGV = chitietkhoahoc.MaGV and khoahoc.MaKH = '$namHoc';";
+                        //  echo $sql_dsMon;
+                        $rs_dsMon = mysqli_query($conn, $sql_dsMon);
+                        if (mysqli_num_rows($rs_dsMon) > 0) {
+                            while ($row_dsMon = mysqli_fetch_assoc($rs_dsMon)) {
+                        ?>
+                                <?php
+                                echo "<a href='chiTietHocPhan.php?idMon=" . $row_dsMon['MaMonHoc'] ."&namhoc=$namHoc"."'>" . $row_dsMon['tenMon'] . "</a>";
+                                echo "<br>";
+                                ?>
+                        <?php
                         }
                     }
                     ?>
